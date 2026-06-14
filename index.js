0f1af670eb0a73e9e7d696fb4d13b12f34fe338b
Astha38 <asthasapre@gmail.com>
2026-06-14 23:18:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
 });
+
+
+
+// const http = require('http');
+
+// // Change { writeHead, end } to just res
+// const server = http.createServer((req, res) => {
+//     console.log('--- url---');
+//     console.log(req.url);
+
+//     console.log('---Method---');
+//     console.log(req.method);
+
+//     console.log('---Headers---');
+//     console.log(req.headers);
+
+//     // Call the methods directly from the res object
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Check your terminal to see the logs!');
+// });
+
+// server.listen(3000, () => {
+//     console.log('Server is running on http://localhost:3000');
+// });
