b31211b082bfb74a4de90f202b72c366c7b86a1e
Astha38 <asthasapre@gmail.com>
2026-06-17 23:02:37 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/',(req, res) => {
     res.send('Hello, Express is up and running!');
 });
 
+app.get('/home',(req, res) => {
+    res.send('This is our home page!');
+});
+
 app.listen(PORT, () =>{
     console.log(`Server is running smoothly on http://localhost:${PORT}`);
 });
+
