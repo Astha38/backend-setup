9b264c1c4d7235d45d9e509dc66fe2d0f7c3bc25
Astha38 <asthasapre@gmail.com>
2026-06-19 15:31:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,30 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/'){
-    res.writeHead(200,{'Content-type': 'text/plain'});
-    res.end('Welcome Home');
-  } else if (req.url === '/about'){
-    res.writeHead(200,{'content-type': 'text/plain'});
-    res.end('this is the about page');
-  } else {
-    res.writeHead(404,{'content-type': 'text/plain'});
-    res.end('404 Not Found')
-  }
+// ✅ Middleware works perfectly here
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.path}`);
+  next();
 });
 
-server.listen(3000, () => {
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome Home');
+});
+
+app.get('/about', (req, res) => {
+  res.send('this is the about page');
+});
+
+app.get('/home', (req, res) => {
+  res.send('Welcome home!');
+});
+
+// 404 Catch-all
+app.use((req, res) => {
+  res.status(404).send('404 Not Found');
+});
+
+app.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
 });
-
-
-
-// const http = require('http');
-
-// // Change { writeHead, end } to just res
-// const server = http.createServer((req, res) => {
-//     console.log('--- url---');
-//     console.log(req.url);
-
-//     console.log('---Method---');
-//     console.log(req.method);
-
-//     console.log('---Headers---');
-//     console.log(req.headers);
-
-//     // Call the methods directly from the res object
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.end('Check your terminal to see the logs!');
-// });
-
-// server.listen(3000, () => {
-//     console.log('Server is running on http://localhost:3000');
-// });
